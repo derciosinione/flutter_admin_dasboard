18f62ceebcb5a31c42692008c8ed0939cc8b980a
derciosinione <derciosinione@gmail.com>
2022-02-06 16:39:24 +0100

    feat: Creating Menu controller

--- lib/controllers/menu_controller.dart
+++ lib/controllers/menu_controller.dart
@@ -1,5 +1,12 @@
+import 'package:flutter_web_dasboard/routes/routes.dart';
 import 'package:get/get.dart';
 
 class MenuController extends GetxController {
   static MenuController instance = Get.find();
+  var activeItem = overViwPageRoute.obs;
+  var hoverItem = "".obs;
+
+  changeActiveItemTo(String itemName){
+    
+  }
 }
