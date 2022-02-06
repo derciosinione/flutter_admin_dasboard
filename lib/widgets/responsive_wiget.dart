dd48e29e8e640062d0624fdff8fc83386133d218
derciosinione <derciosinione@gmail.com>
2022-02-06 13:26:43 +0100

    Crating Responsive Widget

--- lib/widgets/responsive_wiget.dart
+++ lib/widgets/responsive_wiget.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
+const int largeScreenSize = 1366;
+const int mediumScreenSize = 768;
+const int smallScreenSize = 360;
+const int costumScreenSize = 1100;
+
 class ResponsiveWiget extends StatelessWidget {
-  const ResponsiveWiget({Key? key}) : super(key: key);
+  final Widget largeScreen;
+  final Widget? mediumScreen;
+  final Widget? smallScreen;
+  final Widget? costumScreen;
+
+  const ResponsiveWiget(
+      {Key? key,
+      required this.largeScreen,
+      this.mediumScreen,
+      this.smallScreen,
+      this.costumScreen})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
