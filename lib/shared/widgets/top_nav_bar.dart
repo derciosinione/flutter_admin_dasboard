0a85f8f65edd6dc5d7582dba190d090fe88c9ba4
derciosinione <derciosinione@gmail.com>
2022-02-06 15:53:14 +0100

    Creating top nav bar

--- lib/shared/widgets/top_nav_bar.dart
+++ lib/shared/widgets/top_nav_bar.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_dasboard/shared/constants/color.dart';
 import 'package:flutter_web_dasboard/shared/themes/app_images.dart';
 import 'package:flutter_web_dasboard/shared/widgets/responsive_wiget.dart';
 
+import 'custom_text.dart';
+
 AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
     AppBar(
-      backgroundColor: Colors.blue,
       leading: getLeadingIcon(context, key),
       elevation: 0,
       title: Row(
         children: [
-          Visibility(child: CustomText())
+          const Visibility(
+            child: CustomText(
+              text: "Dash",
+              color: lightGrey,
+              size: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(child: Container(color: Colors.red)),
+          IconButton(
+            icon: Icon(Icons.settings, color: dark.withOpacity(.7)),
+            onPressed: () {},
+          ),
+          Stack(
+            children: [
+              IconButton(
+                  icon: Icon(Icons.notifications, color: dark.withOpacity(.7)),
+                  onPressed: () {}),
+              Positioned(
+                top: 7,
+                right: 7,
+                child: Container(
+                  width: 12,
+                  height: 12,
+                  padding: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: active,
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: light, width: 2),
+                  ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
