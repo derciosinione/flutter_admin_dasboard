99f30b09b644c30dbb0e7dbd7bea34750174d598
derciosinione <derciosinione@gmail.com>
2022-02-06 16:52:15 +0100

    feat: Creating Menu controller

--- lib/controllers/menu_controller.dart
+++ lib/controllers/menu_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_web_dasboard/routes/routes.dart';
 import 'package:flutter_web_dasboard/shared/constants/color.dart';
 import 'package:get/get.dart';
@@ -20,7 +21,18 @@ class MenuController extends GetxController {
   bool isHovering(String itemName) => hoverItem.value == itemName;
 
   Widget returnIconFor(String itemName) {
-    return Container();
+    switch (itemName) {
+      case overViwPageRoute:
+        return _customIcon(Icons.trending_up, itemName);
+      case driversPageRoute:
+        return _customIcon(Icons.drive_eta, itemName);
+      case clientsPageRoute:
+        return _customIcon(Icons.people_alt_outlined, itemName);
+      case authenticationPageRoute:
+        return _customIcon(Icons.exit_to_app, itemName);
+      default:
+        return _customIcon(Icons.trending_up, itemName);
+    }
   }
 
   Widget _customIcon(IconData icon, String itemName) {
