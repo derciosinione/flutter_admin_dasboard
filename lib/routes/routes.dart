bd33ab2a0f6904f218e6e63ae31de693341480e7
derciosinione <derciosinione@gmail.com>
2022-02-06 16:34:57 +0100

    Creating pages routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,4 +1,11 @@
-const String overViwPageRoute = "overview";
-const String driversPageRoute = "drivers";
-const String clientsPageRoute = "clients";
-const String authenticationPageRoute = "authentication";
+const String overViwPageRoute = "Overview";
+const String driversPageRoute = "Drivers";
+const String clientsPageRoute = "Clients";
+const String authenticationPageRoute = "Authentication";
+
+List sideMenuItens = [
+  overViwPageRoute,
+  driversPageRoute,
+  clientsPageRoute,
+  authenticationPageRoute
+];
