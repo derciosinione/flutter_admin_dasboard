88d020d6812c7683edf67bce94d3756ffa4b48b8
derciosinione <derciosinione@gmail.com>
2022-02-06 20:08:44 +0100

    feat: Configure Pages Routes

--- lib/helpers/local_navigator.dart
+++ lib/helpers/local_navigator.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_web_dasboard/routes/router.dart';
 import 'package:flutter_web_dasboard/routes/routes.dart';
 import 'package:flutter_web_dasboard/shared/constants/controllers.dart';
 
 Navigator localNavigator() => Navigator(
       key: navigationController.navigatorKey,
       initialRoute: overViwPageRoute,
+      onGenerateRoute: generateRoute,
     );
