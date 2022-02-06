4de876b099c09920beb472e7f88450c0a035e20f
derciosinione <derciosinione@gmail.com>
2022-02-06 16:33:23 +0100

    Creating pages routes

---
+++ lib/routes/routes.dart
@@ -0,0 +1,4 @@
+const String overViwPageRoute = "overview";
+const String driversPageRoute = "drivers";
+const String clientsPageRoute = "clients";
+const String authenticationPageRoute = "authentication";
