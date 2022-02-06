9f720a0cfd13961ab3b96c9615cd326a8b1fab7b
derciosinione <derciosinione@gmail.com>
2022-02-06 13:15:47 +0100

    Crated Small Screen

--- lib/widgets/small_screen.dart
+++ lib/widgets/small_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class SmallScreen extends StatelessWidget {
-  const SmallScreen({ Key? key }) : super(key: key);
+  const SmallScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      constraints: const BoxConstraints.expand(),
+      color: Colors.green,
     );
   }
 }
