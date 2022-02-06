17c9cfd36c9c59b01f026f7f83f614eed6411730
derciosinione <derciosinione@gmail.com>
2022-02-06 19:58:52 +0100

    feat: Created Pages files

---
+++ lib/pages/clients/clients.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Clients extends StatelessWidget {
+  const Clients({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
