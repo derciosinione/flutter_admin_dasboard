475341f1dd9dd0a74d6d03ab74ec15533bfe3eb2
derciosinione <derciosinione@gmail.com>
2022-02-06 20:06:54 +0100

    feat: Created Pages files

---
+++ lib/routes/router.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_web_dasboard/pages/clients/clients.dart';
+import 'package:flutter_web_dasboard/pages/drives/drivers.dart';
+import 'package:flutter_web_dasboard/pages/login/login.dart';
+import 'package:flutter_web_dasboard/pages/overview/overview.dart';
+import 'package:flutter_web_dasboard/routes/routes.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case overViwPageRoute:
+      return _pageRoute(const OverviewPage());
+    case driversPageRoute:
+      return _pageRoute(const DriversPage());
+    case clientsPageRoute:
+      return _pageRoute(const ClientsPage());
+    case authenticationPageRoute:
+    case "Log Out":
+      return _pageRoute(const LoginPage());
+    default:
+      return _pageRoute(const OverviewPage());
+  }
+}
+
+PageRoute _pageRoute(Widget child) {
+  return MaterialPageRoute(builder: (context) => child);
+}
